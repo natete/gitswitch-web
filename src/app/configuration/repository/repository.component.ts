be768dacd939608ae06416bb472d1f32c86342c0
pmsobrado <pmsobrado@emergya.com>
2017-03-16 13:56:11 +0100

    added animations

--- src/app/configuration/repository/repository.component.ts
+++ src/app/configuration/repository/repository.component.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, style, animate, sequence, transition, trigger} from '@angular/core';
 import {Repository} from "./repository";
 
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.scss']
+  styleUrls: ['./repository.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ height: '0', opacity: '0', transform: 'translateY(-100px)' }),
+        sequence([
+          animate(".7s ease", style({ height: '*', opacity: '1', transform: 'translateY(0)' }))
+        ])
+      ]),
+      transition(':leave', [
+        style({ height: '*', opacity: '1', transform: 'translateX(0)'}),
+        sequence([
+          animate(".7s ease", style({ height: '0', opacity: 0, transform: 'translateY(-100px)' }))
+        ])
+      ])
+    ])
+  ]
 })
 export class RepositoryComponent implements OnInit {
 
   @Input() repository: Repository;
+  unfolded: boolean = undefined;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+
+  }
+
+  toggleSubsection() {
+    (this.unfolded && this.unfolded !== undefined) ? this.unfolded = false : this.unfolded = true;
+  }
 }
